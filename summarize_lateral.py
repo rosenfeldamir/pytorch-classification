df8425f731ea7d38d471be2da87d05e8a8e79d4c
rosenfeldamir <amir.rosenfeld@gmail.com>
2018-02-20 21:30:43 -0500

    summary for full runs.

--- summarize_lateral.py
+++ summarize_lateral.py
@@ -1,10 +1,11 @@
 # read logs.
 import pickle
 import os
+from itertools import product
 from os.path import isfile
 results = []
 for trial in range(5):
-    for lateral in ['default','none']:#' random']:
+    for lateral in ['default','none','rand']:#' random']:
         for let_inhibition_learn in [False,True]:
             for the_arch in ['dense','wrn']:
                 for dataset in ['100','10']:
@@ -22,6 +23,36 @@ for trial in range(5):
                     ff = os.path.join(curOutDir,'log.txt')
                     ff = os.path.expanduser(ff)
                     if isfile(ff):
-                        results.append(dict(trial=trial,let_inhibition_learn=let_inhibition_learn,arch=the_arch,dataset=dataset,flines = open(ff).readlines()))
+                        results.append(dict(trial=trial,lateral=lateral,let_inhibition_learn=let_inhibition_learn,arch=the_arch,dataset=dataset,flines = open(ff).readlines()))
 
 pickle.dump(results,open('/data/results.pkl','w'))
+
+
+# read logs for full runs.
+
+results = []
+for trial in range(5):
+    for lateral in ['default','none','rand']:#' random']:
+        for let_inhibition_learn in [False,True]:
+            for the_arch in ['dense','wrn']:
+                for dataset in ['100','10']:
+                
+                    if lateral=='none' and let_inhibition_learn: 
+                        # nothing to learn where there's no "inhibition"
+                        continue 
+                                        
+                    if the_arch == 'dense':
+                        curOutDir = '~/checkpoints/cifar{}_inhibition/densenet-bc-100-12-IN_{}_IL_{}-run_{}'
+                        epochs = 300
+                    elif the_arch == 'wrn':
+                        curOutDir = '~/checkpoints/cifar{}_inhibition/wrn-28-10-IN_{}_IL_{}-run_{}'
+                    
+                    curOutDir = curOutDir.format(dataset,lateral,let_inhibition_learn,trial)
+                    curOutDir+='_FULL'
+                    ff = os.path.join(curOutDir,'log.txt')
+                    ff = os.path.expanduser(ff)
+                    if isfile(ff):
+                        results.append(dict(trial=trial,lateral=lateral,let_inhibition_learn=let_inhibition_learn,arch=the_arch,dataset=dataset,flines = open(ff).readlines()))
+
+pickle.dump(results,open('/data/results_full.pkl','w'))
+
